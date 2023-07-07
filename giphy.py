80f5c73ce9b78ee11d9a972b3c9c761023d86b47
Metasferous <53952788+Metasferous@users.noreply.github.com>
2023-07-08 00:02:45 +0300

    Correction

--- giphy.py
+++ giphy.py
@@ -1,25 +1,27 @@
 import requests
 
-api_key = '7MoBjO3FZ056uWr0Y3cx1RuA08EfMC34'
-query = 'hello'
-limit = '1'
-offset = '0'
-rating = 'g'
-lang = 'en'
-bundle = 'messagin_non_clips'
+API_KEY = '7MoBjO3FZ056uWr0Y3cx1RuA08EfMC34'
+LIMIT = '1'
+OFFSET = '0'
+RATING = 'g'
+LANG = 'en'
+BUNDLE = 'messagin_non_clips'
 
 
 def compose_search_url(search_request: str):
-    url = 'https://api.giphy.com/v1/gifs/search?api_key=' + api_key
+    url = 'https://api.giphy.com/v1/gifs/search?api_key=' + API_KEY
     url += '&q=' + search_request
-    url += '&limit=' + limit
-    url += '&offset=' + offset
-    url += '&rating=' + rating
-    url += '&lang=' + lang
-    url += '&bundle=' + bundle
+    url += '&limit=' + LIMIT
+    url += '&offset=' + OFFSET
+    url += '&rating=' + RATING
+    url += '&lang=' + LANG
+    url += '&bundle=' + BUNDLE
     return url
 
 
 def get_image_url(search_request: str) -> str:
     response = requests.get(compose_search_url(search_request))
-    return response.json()['data'][0]['url']
+    if response.status_code == 200:
+        return response.json()['data'][0]['url']
+    else:
+        return 'Sorry, API isn`t working'
